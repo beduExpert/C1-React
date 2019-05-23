0d7dfff860275da5fb7e040a37cc3e15d9e0848f
Yonatan Rojas S <yonatan@bedu.org>
2019-05-23 00:27:25 -0500

    created post-work whith CRA, updated files

---
+++ Sesion03-componentes-y-ciclo-de-vida/post-work/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>Bienvenido a mi sitio personal</h1>
+      </header>
+      <main>
+        <p>
+          Soy un desarrollador de React que le gusta enforcarse en los estándares de la
+          industria...
+        </p>
+      </main>
+      <footer>
+        <h5><a href="mailto:mi@correo.com">Sigamos en contacto!</a></h5>
+        <h5><a href="https://github.com/miUsuario">Esto es mi trabajo público</a></h5>
+        <h5><a href="https://twitter.com/@miUsuario">Esto pienso</a></h5>
+      </footer>
+    </>
+  );
+}
+
+export default App;
