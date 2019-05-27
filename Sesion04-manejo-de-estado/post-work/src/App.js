d34fbd8251fd2386087df31370682adaa407775d
Yonatan Rojas S <yonatan@bedu.org>
2019-05-26 22:32:17 -0500

    fetched data in App

--- Sesion04-manejo-de-estado/post-work/src/App.js
+++ Sesion04-manejo-de-estado/post-work/src/App.js
@@ -2,15 +2,42 @@ import React from 'react';
 import Header from './Header/Header';
 import Main from './Main/Main';
 import Footer from './Footer/Footer';
+import Card from './Main/Card/Card';
 
-function App() {
-  return (
-    <>
-     <Header />
-     <Main />
-     <Footer />
-    </>
-  );
+class App extends React.Component {
+  state = {
+    data: []
+  };
+
+  componentDidMount() {
+    fetch("https://rickandmortyapi.com/api/character/")
+      .then(r => r.json())
+      .then(r => this.setState((state, props) => ({ data: r.results })));
+    }
+    
+    render() {
+      console.log("data: ", this.state.data);
+      return (
+        <>
+          <Header />
+          <Main>
+            {
+              this.state.data.map(card => {
+                return (
+                  <Card
+                    key={card.id}
+                    image={card.image}
+                    name={card.name}
+                    specie={card.specie}
+                  />
+                );
+              })
+            }
+          </Main>
+          <Footer />
+        </>
+      );
+  }
 }
 
 export default App;
