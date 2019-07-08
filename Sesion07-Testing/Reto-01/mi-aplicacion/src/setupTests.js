648b7c1ab858d315b4e3faa8c23a9cabf0139771
wicho1001 <wichoguerrero@yahoo.com>
2019-07-08 13:03:08 -0500

    Removing challenges of session 7

--- Sesion07-Testing/Reto-01/mi-aplicacion/src/setupTests.js
+++
@@ -1,7 +0,0 @@
-import {configure, shallow, mount} from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
-
-configure({adapter: new Adapter()})
-
-global.shallow = shallow
-global.mount = mount
