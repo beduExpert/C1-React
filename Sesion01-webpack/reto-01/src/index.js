2f2148061c88255390bba5702782e491e8eeee98
wicho1001 <wichoguerrero@yahoo.com>
2019-07-08 13:02:12 -0500

    Removing challenges of session 1

--- Sesion01-webpack/reto-01/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { render } from "react-dom";
-
-import Test from "./components/Test";
-import "./styles.scss";
-
-render(<Test />, document.getElementById("root"));
